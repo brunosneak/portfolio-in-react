b22dc12302b56550e4cf1337cbfbfd99d8fdb37b
bruno <brunosneak@gmail.com>
2020-06-04 22:25:54 +0200

    page home centré

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import Header from "../header/Header";
 import Style from "./Home.module.scss";
-import bgMain from "../../assets/imgAccueil/bgMain.svg";
-import bg1 from "../../assets/imgAccueil/bg1.svg";
-import bg2 from "../../assets/imgAccueil/bg2.svg";
-import bg3 from "../../assets/imgAccueil/bg3.svg";
-import bg4 from "../../assets/imgAccueil/bg4.svg";
-import bg5 from "../../assets/imgAccueil/bg5.svg";
-import bg6 from "../../assets/imgAccueil/bg6.svg";
 import croix from "../../assets/imgAccueil/croix.svg";
 import flecheDroite from "../../assets/imgAccueil/flecheDroite.svg";
 import flecheGauche from "../../assets/imgAccueil/flecheGauche.svg";
@@ -44,13 +37,13 @@ class Home extends Component {
             <div className={ Style.container }>
                 <Header/>
                 <section>
-                    <img src={ bgMain } className={ Style.bgMain } alt="main-background"/>
-                    <img src={ bg1 } className={ Style.bg1 } alt="tree-background"/>
-                    <img src={ bg2 } className={ Style.bg2 } alt="skull-background"/>
-                    <img src={ bg3 } className={ Style.bg3 } alt="leaf-background"/>
-                    <img src={ bg4 } className={ Style.bg4 } alt="worms-background"/>
-                    <img src={ bg5 } className={ Style.bg5 } alt="platform-background"/>
-                    <img src={ bg6 } className={ Style.bg6 } alt="wall-background"/>
+                    <div className={ Style.bgMain }/>
+                    <div className={ Style.bg1 }/>
+                    <div className={ Style.bg2 }/>
+                    <div className={ Style.bg3 }/>
+                    <div className={ Style.bg4 }/>
+                    <div className={ Style.bg5 }/>
+                    <div className={ Style.bg6 }/>
                 </section>
 
                 <div className={ Style.masquePerso }>
